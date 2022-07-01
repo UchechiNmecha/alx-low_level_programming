18521abc6b93ac0c32b63dcc5d25acfb5d781546
Uchechi-Nmecha <nmechauchechiokorie@gmail.com>
2022-06-30 21:31:07 -0700

    This is task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,6 @@ int main(void)
 	printf("Size of an int: %lu byte(s)\n", sizeof(in));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(lng));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(lolng));
-	printf("Size of a float: %lu byte(s)\n", sizeof(flt));
-	
+	printf("Size of a float: %lu byte(s)\n", sizeof(flt));	
 	return (0);
 }
