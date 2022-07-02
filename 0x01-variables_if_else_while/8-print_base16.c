6a85b4277f1891592ba897e9de8eaef8d6d2e84a
Uchechi-Nmecha <nmechauchechiokorie@gmail.com>
2022-07-01 17:21:15 -0700

    This is task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0 (success)
+**/
+int main(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+	for (num = 'a'; num <= 'f'; num++)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}
