afe154c513c277999572f574a3e17be4ff51bed9
Uchechi-Nmecha <nmechauchechiokorie@gmail.com>
2022-07-05 16:14:02 -0700

    This is task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _abs - Entry point, prints absolute value
+* @i: integer
+* Return: absolute value of n
+*/
+
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (-i);
+	}
+}
