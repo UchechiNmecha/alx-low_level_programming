09bc50ec4f978aa62ae28c9e1959e6ede094cb7f
Uchechi-Nmecha <nmechauchechiokorie@gmail.com>
2022-07-05 16:45:54 -0700

    This is task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* add - adds teo integers
+* @num1: integer
+* @num2: integer
+* Return: sum
+*/
+
+int add(int num1, int num2)
+{
+	int result;
+
+	result = num1 + num2;
+
+	return (result);
+}
