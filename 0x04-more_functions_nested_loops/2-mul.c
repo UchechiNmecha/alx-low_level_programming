4c2d1b9e7a3e03ca5d5b5f1ec15ffe28177c4d2f
Uchechi-Nmecha <nmechauchechiokorie@gmail.com>
2022-07-07 07:51:52 -0700

    This is task 2, calculates the product of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * @a: first integer
+ * @b: second integer
+ * Return: product of two integers
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
