8480236caddad3ea00d1e92cdbd8e4f560f32467
Uchechi-Nmecha <nmechauchechiokorie@gmail.com>
2022-07-07 08:44:05 -0700

    This is task 6, prints straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,20 +7,21 @@
  */
 
 void print_line(int n)
+
 {
+
 	int num;
 
-	num = 0;
-	if (n > num)
+	if (n <= 0)
 	{
-		for (i = 0; i <= n; i++)
-		{
-		_putchar('_');
-		}
-	_putchar('\n');
+		_putchar('\n');
 	}
 	else
 	{
-	_putchar('\n');
+		for (num = 1; num <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
 	}
 }
