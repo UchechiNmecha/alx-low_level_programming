6c30d0b49c2f467bd6a3898ae931b43ac79cc0cb
Uchechi-Nmecha <nmechauchechiokorie@gmail.com>
2022-07-07 08:10:02 -0700

    This is task 4, prints numbers to 9 except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry point
+ * Return: empty
+ */
+
+void print_most_numbers(void)
+{
+	int num;
+
+	num = 0;
+	while (num <= 9)
+	{
+		if (num != 2 && num != 4)
+		{
+			_putchar(num + '0');
+		}
+		num++;
+	}
+	_putchar('\n');
+}
