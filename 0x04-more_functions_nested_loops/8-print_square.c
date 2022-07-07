c41a464faeba0e019ad6bc70886c296e67508776
Uchechi-Nmecha <nmechauchechiokorie@gmail.com>
2022-07-07 09:02:34 -0700

    This is task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - Entry point
+ * @size: size
+ */
+
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 1; a <= size; a++)
+		{
+			for (b = 1; b <= size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
