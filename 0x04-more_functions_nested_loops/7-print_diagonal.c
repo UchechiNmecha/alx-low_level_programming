2d5ba9a757a65564cb180b1ce955841cf93492e5
Uchechi-Nmecha <nmechauchechiokorie@gmail.com>
2022-07-07 08:53:56 -0700

    This is task 7, prints diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Entry point
+ * @n: number of times to print '\'
+ * Return: diagonal
+ */
+void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 1; a <= n; a++)
+		{
+			for (b = 1; b < a; b++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
