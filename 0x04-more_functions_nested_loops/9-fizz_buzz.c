4dd4068fba5ff4316a59390a6fb4677d3e9a46f1
Uchechi-Nmecha <nmechauchechiokorie@gmail.com>
2022-07-07 11:37:07 -0700

    This is task 9, FizzBuzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,25 +14,22 @@ int main(void)
 	{
 		if (num % 3 == 0 && num % 5 == 0)
 		{
-			printf("FizzBuzz");
-			printf(" ");
+			printf("FizzBuzz ");
 		}
 		else if (num % 3 == 0)
 		{
-			printf("Fizz");
-			printf(" ");
+			printf("Fizz ");
 		}
 		else if (num % 5 == 0)
 		{
-			printf("Buzz");
-			printf(" ");
+			printf("Buzz ");
 		}
 		else
 		{
-			printf("%d", num);
-			printf(" ");
+			printf("%d ", num);
 		}
 	}
 	printf("\n");
+
 	return (0);
 }
