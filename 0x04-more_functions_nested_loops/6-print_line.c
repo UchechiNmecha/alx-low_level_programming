590162135b6935ba18e6a6fe312811c73e43b2af
Uchechi-Nmecha <nmechauchechiokorie@gmail.com>
2022-07-07 08:39:37 -0700

    This is task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ * @n: length
+ * Return: empty
+ */
+
+void print_line(int n)
+{
+	int num;
+
+	num = 0;
+	if (n > num)
+	{
+		for (i = 0; i <= n; i++)
+		{
+		_putchar('_');
+		}
+	_putchar('\n');
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
