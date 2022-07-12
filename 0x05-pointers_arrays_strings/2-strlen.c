a8f44ec41108dd096df76ac50fe17cbee816b453
Uchechi-Nmecha <nmechauchechiokorie@gmail.com>
2022-07-12 16:50:35 -0700

    This is task 2, a function that prints the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - Entry point, a function that returns the length of a string
+ * @s: string input
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int num;
+
+	while (s[num] != '\0')
+	{
+		num++;
+	}
+	return (num);
+}
