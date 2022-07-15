f464b6b07db6c68268d59c30ba83dec34ca888aa
Uchechi-Nmecha <nmechauchechiokorie@gmail.com>
2022-07-15 15:40:42 -0700

    This is task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - Entry point
+ * @n: integer to be printed
+ */
+void print_number(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	else if (n > 0)
+		n *= -1;
+	else
+		_putchar('-');
+	if ((n / 10) != 0)
+	{
+		print_number((n / 10) * -1);
+	}
+	_putchar((n % 10) * -1 + '0');
+}
