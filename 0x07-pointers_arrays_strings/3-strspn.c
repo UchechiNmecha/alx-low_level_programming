2cf2d30e54344fb0269995d90cf1de87ac82f54a
Uchechi-Nmecha <nmechauchechiokorie@gmail.com>
2022-07-19 11:26:07 -0700

    This is task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - Gets the length of a prefix substring
+ * @s: Character to be printed
+ * @accept: Character
+ * Return: length
+ **/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != s[i]; j++)
+		{
+			if (accept[j] == '\0')
+				return (i);
+		}
+	}
+	return (i);
+}
