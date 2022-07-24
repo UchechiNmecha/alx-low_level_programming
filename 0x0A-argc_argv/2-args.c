b5b857e55c650f1807d16ccb6e89d820a7d97b8b
Uchechi-Nmecha <nmechauchechiokorie@gmail.com>
2022-07-24 04:01:18 -0700

    This is task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point, prints all arguments passed
+ * @argc: argument count
+ * @argv: array of commands
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
