8aa5ed32148d576de8dc77d19905bcd0863696da
Uchechi-Nmecha <nmechauchechiokorie@gmail.com>
2022-08-23 18:21:46 -0700

    File I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,30 +1,31 @@
 #include "main.h"
-
 /**
- * append_text_to_file - Append text to the end of a file
- * @filename: file
- * @text_content: string to add
- * Return: Always 1(Success), otherwise -1
- */
+* append_text_to_file- Entry point
+* @filename: file to create
+* @text_content: null term
+* Return: 1 success, -1 fail
+*/
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fol, status, i;
+	int fol, siz = 0, wir;
 
 	if (filename == NULL)
 		return (-1);
+	fol = open(filename, O_RDWR | O_APPEND, S_IRGRP | S_IWGRP | S_IROTH);
+	if (fol == -1)
+		return (-1);
+
 	if (text_content == NULL)
-		return (1);
+		text_content = "";
+	while (text_content[sz] != '\0')
+		siz++;
 
-	fol = open(filename, O_APPEND | O_WRONLY);
-	if (fd == -1)
-		return (-1);
+	wir = write(fol, text_content, siz);
 
-	for (i = 0; text_content[i] != '\0'; i++)
-		;
-	status = write(fd, text_content, i);
-	if (status == -1)
-		return (-1);
+	if (wir == -1)
+	return (-1);
 
 	close(fol);
 	return (1);
+
 }
